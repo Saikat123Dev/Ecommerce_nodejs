54ee4bef428ab938a7b96b7c88e8caf08245b0ef
Saikat123Dev <berasaikat729@gmail.com>
2024-04-15 16:45:51 +0530

    first

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+
+
+const Login = () => {
+  return (
+    <section id="login">
+          <div className="max-auto container px-4">
+
+          </div>
+    </section>
+  )
+}
+
+export default Login
